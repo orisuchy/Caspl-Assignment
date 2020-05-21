3511886f4c68879c0e11ac604f084b2d0d042a29
Ori <suchy@post.bgu.ac.il>
2020-05-21 10:45:33 +0300

    try

---
+++ Ass2/calc.s
@@ -0,0 +1,13 @@
+section .text
+  align 16
+  global main
+  extern printf
+  extern fprintf 
+  extern fflush
+  extern malloc 
+  extern calloc 
+  extern free 
+  extern gets 
+  extern getchar 
+  extern fgets 
+main:
