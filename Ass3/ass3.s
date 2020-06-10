51b5182334e2e55e34464a39d31a18436ebc5864
Ori <suchy@post.bgu.ac.il>
2020-06-10 20:33:27 +0300

    ass3.s init sections

---
+++ Ass3/ass3.s
@@ -0,0 +1,13 @@
+section	.rodata
+section .bss
+section .data
+section .text
+    global main
+    extern printf
+    extern fprintf
+    extern sscanf
+    extern malloc
+    extern calloc
+    extern free
+main:
+    
